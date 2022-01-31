a4da298dca36a4208b31bfb1104ad54c566cb69c
Rajiv-Kinyanjui <peterkinyanjui226@gmail.com>
2022-01-31 23:50:37 +0300

    Prompts user for their age and returns year of birth and when the user will turn 80

---
+++ birthday.py
@@ -0,0 +1,12 @@
+print("Hello!! We would Like to know your birthday. Please press 1 to continue!")
+number = int(input())
+
+print("How old are you")
+age = int(input())
+
+year = 2022 - age
+
+future = 2022 + (80-age)
+
+print(year)
+print(future)
